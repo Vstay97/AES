26742de7971da60333f2bc6dd63648eb27f14a16
Vsaty <583429957@qq.com>
2022-05-13 21:51:20 +0800

    更新脚本

---
+++ run.sh
@@ -0,0 +1,2 @@
+# 用法 sh run.sh
+nohup python main.py -tr data/fold_0/train.tsv -tu=data/fold_0/dev.tsv -ts=data/fold_0/test.tsv --emb ./SWEM/glove.6B.300d.txt --vocab-path output_dir/vocab.pkl --tsp data/training_set_rel3.xls --pp data/prompt.xlsx --swp data/stopword.txt -o output_dir -b=2 -p=0 -g=1 -lr=1e-5 -t ChinaAI --epochs=20 --explain="不要门控" > filelog.txt 2>&1 &
